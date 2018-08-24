3eec294c58b20cb9506c0f51b4fe8dc093619bc2
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-08-23 21:05:12 -0500

    tiny fixes

--- Tutorial/src/app/app.module.ts
+++ Tutorial/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
